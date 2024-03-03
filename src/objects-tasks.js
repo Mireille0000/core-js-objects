d875921483290a4a87b73f7ecb241059957f3c47
Gasanova Tamara <solas93@mail.ru>
2024-03-03 16:20:56 +0300

    feat: implement compareObjects function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -77,8 +77,10 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const objToStr1 = JSON.stringify(obj1);
+  const objToStr2 = JSON.stringify(obj2);
+  return objToStr1 === objToStr2;
 }
 
 /**
